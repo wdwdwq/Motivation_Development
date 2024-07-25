40d26694e5098b9d69eff499f34e0e97eef6f632
wdwdwq <qkenr7278@gmail.com>
2024-07-25 16:48:08 +0900

    12강, Rq 파싱을 모든 명령어에 도입, 라우팅 코드 간결화

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,39 +6,41 @@ import org.example.controller.SystemController;
 
 
 public class App {
+
+    byte system_status = 1;
     public void run() {
         System.out.println("== motivation execution ==");
 
         SystemController systemController = new SystemController();
         MotivationController motivationController = new MotivationController();
 
-        while (true) {
+        while (system_status == 1) {
             System.out.print("command) ");
             String cmd = Container.getScanner().nextLine().trim();
 
-            if (cmd.equals("exit")) {
-                systemController.exit();
-                break;
-            } else if (cmd.length() == 0) {
+            if (cmd.length() == 0) {
                 System.out.println("명령어 입력해");
                 continue;
             }
 
-            if (cmd.equals("add")) {
-                motivationController.add();
-            } else if (cmd.equals("list")) {
-                motivationController.list();
-            } else if (cmd.equals("delete")) {
+            Rq rq = new Rq(cmd);
+
+            switch (rq.getActionMethod()) {
+                case "exit":
+                    systemController.exit();
+                    system_status = 0;
+                    break;
+                case "add":
+                    motivationController.add();
+                    break;
+                case "list":
+                    motivationController.list();
+                    break;
+                case "delete":
                 motivationController.delete();
-
-                Rq rq = new Rq(cmd);
-
-                System.out.println(rq.getActionMethod());
-                System.out.println(rq.getParams("id"));
-                System.out.println(rq.getParams("source"));
-                System.out.println(rq.getParams("motivation"));
-            } else {
-                System.out.println("사용할 수 없는 명령어입니다");
+                default:
+                    System.out.println("사용할 수 없는 명령어입니다");
+                    break;
             }
         }
     }
